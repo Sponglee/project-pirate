e063de9c43f683756a5250fe82687aad2c3ed29b
SolidHinken <acearutyunyan@gmail.com>
2023-03-12 18:37:06 +0400

    Bug fixes

--- Assets/Scripts/UI/DamageUI/DamageUIController.cs
+++ Assets/Scripts/UI/DamageUI/DamageUIController.cs
@@ -1,13 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
+using Anthill;
+using Anthill.Core;
+using Anthill.Inject;
 using UnityEngine;
 
-public class DamageUIController : MonoBehaviour
+public class DamageUIController : ISystem
 {
-    public DamageUIView view;
+    [Inject] public Game Game { get; set; }
 
-    public void RequestNumber()
+    private DamageUIView _view;
+    private bool _isHasHandlers;
+    public void AddedToEngine()
     {
+        AntInject.Inject<DamageUIController>(this);
+        _view = ObjectCreator.Create<DamageUIView>("UI/DamageUIView/DamageUIView", Game.UIRoot);
+
+        AddHandlers();
+        _view.Hide();
+    }
+
+    public void RemovedFromEngine()
+    {
+        RemoveHandlers();
+        GameObject.Destroy(_view.gameObject);
+    }
+
+    private void AddHandlers()
+    {
+        if (_isHasHandlers) return;
+        _isHasHandlers = true;
 
     }
+
+    private void RemoveHandlers()
+    {
+        if (!_isHasHandlers) return;
+        _isHasHandlers = false;
+    }
+
+    public void Show()
+    {
+        _view.Show();
+    }
+
+    public void Hide()
+    {
+        _view.Hide();
+    }
+
+
+
+
+
 }
--- Assets/Scripts/UI/DamageUI/DamageUIView.cs
+++ Assets/Scripts/UI/DamageUI/DamageUIView.cs
@@ -1,10 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class DamageUIView : MonoBehaviour
 {
     public GameObject damageItem;
 
+    private bool _isHasHandlers;
+
+
+    public void Show()
+    {
+        if (_isHasHandlers) return;
+        _isHasHandlers = true;
+
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        if (!_isHasHandlers) return;
+        _isHasHandlers = false;
+
+        gameObject.SetActive(false);
+    }
 
 }
